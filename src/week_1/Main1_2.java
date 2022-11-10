3f267b02450eecac2ebdf61df66235568400911e
Lee Sol <leeso4567@naver.com>
2022-11-10 09:31:02 +0900

    find frequent word algorithm

--- src/week_1/Main1_2.java
+++ src/week_1/Main1_2.java
@@ -1,7 +1,27 @@
 package week_1;
 
 public class Main1_2 { //최빈값 찾기
-    public static void main(String[] args) {
 
+    public static char solution(String str) {
+        str = str.replace(" ","");
+        char answer = ' ';
+        int count = 0;
+        int[] alpha = new int[26];
+
+        for(int i = 0; i < str.length(); i++) {
+            int x = str.charAt(i) - 97;
+            alpha[x]++;
+            if(alpha[x] > count) {
+                count = alpha[x];
+                answer = str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        String str = "hello my name is sparta";
+
+        System.out.println(solution(str));
     }
 }
