99f993ed3e57024b6af2ff44c4e803250c2b30e6
Lee Sol <leeso4567@naver.com>
2022-11-10 10:51:22 +0900

    make max num by plus or multiple

---
+++ src/week_1/Main1_4.java
@@ -0,0 +1,25 @@
+package week_1;
+
+public class Main1_4 {
+    public static int solution(int[] arr) {
+        int answer = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            if (answer == 0 || answer == 1) {
+                answer += arr[i];
+            }
+            else if(arr[i] == 1) {
+                answer += arr[i];
+            }
+            else answer *= arr[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 3, 5, 6, 1, 2, 4};
+
+        System.out.println(solution(arr));
+    }
+}
