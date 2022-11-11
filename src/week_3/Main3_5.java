1a6355412743781d629fccd3d4aa19d7fa297b0d
Lee Sol <leeso4567@naver.com>
2022-11-11 21:02:05 +0900

    erge sort algorithm first commit

--- src/week_3/Main3_5.java
+++ src/week_3/Main3_5.java
@@ -1,4 +1,39 @@
 package week_3;
 
-public class Main3_5 {
+public class Main3_5 { //병합 정렬 구현하기
+    public static int[] merge(int[] a, int[] b) {
+        int[] c = new int[a.length + b.length];
+        int a_index = 0;
+        int b_index = 0;
+        int c_index = 0;
+        while (a_index < a.length && b_index < b.length) {
+            if(a[a_index] < b[b_index])
+                c[c_index++] = a[a_index++];
+            else
+                c[c_index++] = b[b_index++];
+        }
+        if(a_index < a.length) {
+            for(int i = a_index; i < a.length; i++)
+                c[c_index++] = a[i];
+        }
+        if(b_index < b.length) {
+            for(int i = b_index; i < b.length; i++)
+                c[c_index++] = b[i];
+        }
+
+        return c;
+    }
+
+    public static int[] merge_sort(int[] arr) {
+        int mid = arr.length / 2;
+        //병합정렬 코드 구현하기
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 2, 1, 6, 8, 7, 4};
+
+        for(int x : merge_sort(arr))
+            System.out.println(x);
+    }
 }
